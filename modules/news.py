8a7f0371b39e24f3c71189dd016cd5c457f6129d
Sharon Zacharia <appuz2849@gmail.com>
2019-04-17 14:19:35 +0530

    Update news.py

--- modules/news.py
+++ modules/news.py
@@ -1,10 +1,10 @@
-# b1701105e13e42668eb9d08a9a976920
+
 
 import requests,json
 
 def getNews():
     try:
-        url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=b1701105e13e42668eb9d08a9a976920'
+        url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=APIKEY'
         response = requests.get(url)
         datas = response.json()
         return datas['articles']
